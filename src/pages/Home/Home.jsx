76136d1108deaf0b541480492d7fbc079b787652
Mehedi Hasan <89673282+MehediMashfan086@users.noreply.github.com>
2023-07-26 17:55:16 +0600

    feat(router): route link for app component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Home.css"
+import Header from '../../components/Header/Header'
 
 const Home = () => {
   return (
-    <div>
+    <>
+      <Header />
       Home
-    </div>
+    </>
   )
 }
 
